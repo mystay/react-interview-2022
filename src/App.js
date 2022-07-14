6bd4786fb284d60a6ea29cc73dd58803167a1299
Munien <dillonmunien.dev@mailbox.org>
2022-07-14 16:13:19 +1000

    Add challenge

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
-function App() {
+const App = () => {
+  const [creating, setCreating] = useState(false)
+  const [notes, setNotes] = React.useState([
+    { title: 'test', content: 'test' }
+  ])
+
+  const makeNote = ({ note }) => { setNotes([...notes, note]) }
+
+  function submitForm(e) {
+    e.preventDefault();
+    var formData = new FormData(e.target), formentries = Object.fromEntries(formData);
+    let Note = { title: formentries['title'], content: formentries['content'] }
+
+    makeNote(Note)
+    setCreating(false)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div class="d-flex flex-column p-5">
+    <div >
+    <section style={{ display: 'flex' }}>
+    <h1 style={{ flex: 1 }}>React App</h1>
+    <button   class="addNote btn btn-primary" onClick={() => setCreating(true)} >Add Note
+    </section>
     </div>
+    {creating ? (
+      <div class="mt-3">
+      <h2>Create Note</h2>
+      <hr />
+        <form onSubmit={submitForm} style={{ paddingTop: 10, maxWidth: 500 }}>
+          <label>Title</label>
+          <div style={{ marginBottom: 10 }}></div>
+          <input className="form-control" id="title" name="title" />
+          <div style={{ marginBottom: 10 }}></div>
+          <label>Content</label>
+          <div style={{ marginBottom: 10 }}></div>
+          <input className="form-control "id="content" name="content" />
+          <div style={{ marginBottom: 10 }}></div>
+        <input type="submit" value="submit" />
+        </form>
+      </div>
+    ) : (
+      <div class="mt-3">
+      <h2>My Notes</h2>
+      <hr>
+      {notes.map(function(note) { return (
+        <div className="d-flex"><p className="me-3"><span style={{ fontWeight: 'bold' }}>Title:</span> {note.title}</p><p><strong>Content:</strong> {note.contents}</p></div>
+      ) })}
+      </div>
+    )}
+    <div>
+    </div>
+  </div>
   );
 }
 
